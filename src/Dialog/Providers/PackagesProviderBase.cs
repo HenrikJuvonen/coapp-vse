c677c33115f00426d84ad677323ccc5bff770c9c
Henrik Juvonen <henjuv@utu.fi>
2012-06-12 14:17:27 +0300

    ...

--- src/Dialog/Providers/PackagesProviderBase.cs
+++ src/Dialog/Providers/PackagesProviderBase.cs
@@ -395,7 +395,7 @@ namespace CoApp.VisualStudio.Dialog.Providers
                 if (e.Cancelled)
                 {
                     CloseProgressWindow();
-                    _providerServices.UserNotifierServices.ShowErrorMessage("Error occured during the operation. File might not exist in the server.");
+                    //_providerServices.UserNotifierServices.ShowErrorMessage("Error occured during the operation. File might not exist in the server.");
                 }
                 else
                 {
